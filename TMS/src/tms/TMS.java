18d5078b15e10510721ff87322f16747c8b20363
unknown <2ndyrGroupB@PN09.pn.local>
2019-05-18 20:13:35 +0800

    First time git in java

---
+++ TMS/src/tms/TMS.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tms;
+
+import gui.signUp;
+
+/**
+ *
+ * @author student
+ */
+public class TMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        signUp su = new signUp();
+        su.setVisible(true);
+    }
+    
+}
---
+++ TMS/src/tms/Tasks.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tms;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author 2ndyrGroupB
+ */
+public class Tasks {
+    
+    private int TaskID;
+    private String TaskName;
+    private int Tasksize;
+  
+
+    /**
+     * @return the itemID
+     */
+    
+    public int getTaskID() {
+        return TaskID;
+    }
+
+    public Tasks(int id, String name, int size) {
+        this.TaskID = id;
+        this.TaskName = name;
+        this.Tasksize = size;
+    }
+
+    public void setTaskID(int TaskID) {
+        this.TaskID = TaskID;
+    }
+
+    public String getTaskName() {
+        return TaskName;
+    }
+
+    public void setTaskName(String TaskName) {
+        this.TaskName = TaskName;
+    }
+
+    public int getTasksize() {
+        return Tasksize;
+    }
+
+    public void setTasksize(int Tasksize) {
+        this.Tasksize = Tasksize;
+    }
+
+   
+   
+    public Object[] toArray(){
+        Object[] itemArr = new Object[3];
+        itemArr[0] = this.getTaskID();
+        itemArr[1] = this.getTaskName();
+        itemArr[2] = this.getTasksize();
+        
+        return itemArr;
+    }
+}
